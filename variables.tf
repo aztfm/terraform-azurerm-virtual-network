39f038e538738d07db1349779472c56c035a6939
gareda <gareda95@outlook.es>
2020-12-25 12:10:43 +0100

    Application of good practices in variables: quotation marks

--- variables.tf
+++ variables.tf
@@ -1,36 +1,36 @@
-variable name {
+variable "name" {
   type        = string
   description = "The name of the virtual network."
 }
-variable resource_group_name {
+variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the virtual network."
 }
-variable location {
+variable "location" {
   type        = string
   description = "The location/region where the virtual network is created."
 }
-variable address_space {
+variable "address_space" {
   type        = list(string)
   description = "The address space that is used in the virtual network. More than one address space can be provisioned."
 }
-variable dns_servers {
+variable "dns_servers" {
   type        = list(string)
   default     = []
   description = "List of IP addresses of DNS servers."
 }
-variable ddos_protection_plan_id {
+variable "ddos_protection_plan_id" {
   type        = string
   default     = null
   description = "The ID of DDoS Protection Plan."
 }
-variable subnets {
+variable "subnets" {
   type        = list(map(string))
   default     = []
   description = "List of objects that represent the configuration of each subnet."
   # subnets = [{ name = "", address_prefix = "" }]
 }
-variable tags {
+variable "tags" {
   type        = map(string)
   default     = {}
   description = "A mapping of tags to assign to the resource."
