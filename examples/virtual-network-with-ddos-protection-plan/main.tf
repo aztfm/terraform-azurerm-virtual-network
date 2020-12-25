7d4c50150350302b20ff2aa292462551c24ef9fb
gareda <gareda95@outlook.es>
2020-12-25 12:05:33 +0100

    Update example virtual-network-with-ddos-protection-plan

--- examples/virtual-network-with-ddos-protection-plan/main.tf
+++ examples/virtual-network-with-ddos-protection-plan/main.tf
@@ -2,23 +2,24 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "virtual-network" {
+resource "azurerm_resource_group" "rg" {
   name     = "terraform-azurerm-virtual-network"
   location = "West Europe"
 }
 
-resource "azurerm_network_ddos_protection_plan" "virtual-network" {
-  name                = "virtual-network-ddos"
-  resource_group_name = azurerm_resource_group.virtual-network.name
-  location            = azurerm_resource_group.virtual-network.location
+resource "azurerm_network_ddos_protection_plan" "ddos" {
+  name                = "ddos_protection_plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 }
 
 module "virtual-network" {
-  source                  = "./terraform-azurerm-virtual-network"
-  name                    = "virtual-network-vnet"
-  resource_group_name     = azurerm_resource_group.virtual-network.name
-  location                = azurerm_resource_group.virtual-network.location
+  source                  = "aztfm/virtual-network/azurerm"
+  version                 = ">=1.0.0"
+  name                    = "virtual-network"
+  resource_group_name     = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
   address_space           = ["10.0.0.0/16"]
-  ddos_protection_plan_id = azurerm_network_ddos_protection_plan.virtual-network.id
+  ddos_protection_plan_id = azurerm_network_ddos_protection_plan.ddos.id
   subnets                 = [{ name = "subnet1", address_prefix = "10.0.0.0/24" }]
 }
