a2f59f24601e191ca3f846aafbf469876ccfe3cb
gareda <gareda95@outlook.es>
2020-12-25 12:07:09 +0100

    Update example virtual-network-with-multiple-subnets

--- examples/virtual-network-with-multiple-subnets/main.tf
+++ examples/virtual-network-with-multiple-subnets/main.tf
@@ -2,16 +2,17 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "virtual-network" {
+resource "azurerm_resource_group" "rg" {
   name     = "terraform-azurerm-virtual-network"
   location = "West Europe"
 }
 
-module "virtual-network" {
-  source              = "./terraform-azurerm-virtual-network"
-  name                = "virtual-network-vnet"
-  resource_group_name = azurerm_resource_group.virtual-network.name
-  location            = azurerm_resource_group.virtual-network.location
+module "vnet" {
+  source              = "aztfm/virtual-network/azurerm"
+  version             = ">=1.0.0"
+  name                = "virtual-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
   subnets = [
     { name = "subnet1", address_prefix = "10.0.0.0/24" },
