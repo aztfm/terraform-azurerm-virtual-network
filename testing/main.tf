f61ffaf8533be84633fc868064b0895482c7dd1c
Daniel Garrido <gareda95@outlook.es>
2021-06-11 19:36:49 +0200

    Pipeline experimental

--- testing/main.tf
+++ testing/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "laboratory-virtual-network"
+  name     = "terraform-azurerm-virtual-network"
   location = "West Europe"
 }
 
@@ -15,6 +15,7 @@ module "virtual_network" {
   resource_group_name     = azurerm_resource_group.rg.name
   location                = azurerm_resource_group.rg.location
   address_space           = ["10.1.0.0/16", "10.2.0.0/16", "10.3.0.0/16"]
+  dns_servers             = ["8.8.8.8", "8.8.4.4"]
   ddos_protection_plan_id = azurerm_network_ddos_protection_plan.ddos.id
   subnets = [
     { name = "subnet1", address_prefixes = ["10.1.0.0/24"] },
