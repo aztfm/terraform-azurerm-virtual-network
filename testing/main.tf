8baf9536de0780239cb5fe4663d3539c30a61651
Daniel Garrido <gareda95@outlook.es>
2021-06-11 19:27:22 +0200

    Change path module

--- testing/main.tf
+++ testing/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_ddos_protection_plan" "ddos" {
 }
 
 module "virtual_network" {
-  source                  = "./module"
+  source                  = "./virtual-network"
   name                    = "virtual-network"
   resource_group_name     = azurerm_resource_group.rg.name
   location                = azurerm_resource_group.rg.location
