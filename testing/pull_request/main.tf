940df1eccc6a518c387f3e232515e61df5d76086
Daniel Garrido <gareda95@outlook.es>
2021-06-27 16:05:05 +0200

    Set tests for version 0.13.X Release Fix

--- testing/pull_request/main.tf
+++ testing/pull_request/main.tf
@@ -15,8 +15,8 @@ module "virtual_network" {
   address_space       = ["10.1.0.0/16", "10.2.0.0/16", "10.3.0.0/16"]
   dns_servers         = ["8.8.8.8", "8.8.4.4"]
   subnets = [
-    { name = "subnet1", address_prefixes = "10.1.0.0/24" },
-    { name = "subnet2", address_prefixes = "10.2.0.0/24", service_endpoints = "Microsoft.Sql" },
-    { name = "subnet3", address_prefixes = "10.3.0.0/24", service_endpoints = "Microsoft.Storage,Microsoft.Web" }
+    { name = "subnet1", address_prefix = "10.1.0.0/24" },
+    { name = "subnet2", address_prefix = "10.2.0.0/24", service_endpoints = "Microsoft.Sql" },
+    { name = "subnet3", address_prefix = "10.3.0.0/24", service_endpoints = "Microsoft.Storage,Microsoft.Web" }
   ]
 }
