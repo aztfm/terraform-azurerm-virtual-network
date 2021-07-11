26e2e61f524cadea65bda3c9e463abad73b5c6f4
Daniel Garrido <gareda95@outlook.es>
2021-07-11 11:02:25 +0200

    Multi-subnet example update

--- examples/virtual-network-with-multiple-subnets/main.tf
+++ examples/virtual-network-with-multiple-subnets/main.tf
@@ -9,14 +9,14 @@ resource "azurerm_resource_group" "rg" {
 
 module "vnet" {
   source              = "aztfm/virtual-network/azurerm"
-  version             = ">=1.2.0"
+  version             = ">=1.3.0"
   name                = "virtual-network"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
   subnets = [
-    { name = "subnet1", address_prefix = "10.0.0.0/24" },
-    { name = "subnet2", address_prefix = "10.1.0.0/24", service_endpoints = "Microsoft.Sql" },
-    { name = "subnet3", address_prefix = "10.2.0.0/24", service_endpoints = "Microsoft.Storage,Microsoft.Web" }
+    { name = "subnet1", address_prefix = "10.1.0.0/24", delegation = "Microsoft.Sql/managedInstances" },
+    { name = "subnet2", address_prefix = "10.2.0.0/24", service_endpoints = "Microsoft.Sql" },
+    { name = "subnet3", address_prefix = "10.3.0.0/24", service_endpoints = "Microsoft.Storage,Microsoft.Web", delegation = "Microsoft.Web/serverFarms" }
   ]
 }
