352e7eb8a22385f5354696bb743932e2c6a9d412
Daniel Garrido <gareda95@outlook.es>
2021-07-22 16:48:20 +0200

    Fix test pull request

--- testing/pull_request/main.tf
+++ testing/pull_request/main.tf
@@ -11,8 +11,8 @@ module "virtual_network" {
   address_space       = ["10.1.0.0/16", "10.2.0.0/16", "10.3.0.0/16"]
   dns_servers         = ["8.8.8.8", "8.8.4.4"]
   subnets = [
-    { name = "subnet1", address_prefix = ["10.1.0.0/24"], delegation = "Microsoft.ApiManagement/service", enforce_private_link_service_network_policies = true },
-    { name = "subnet2", address_prefix = ["10.2.0.0/24"], service_endpoints = ["Microsoft.Sql"], enforce_private_link_endpoint_network_policies = true },
-    { name = "subnet3", address_prefix = ["10.3.0.0/24"], service_endpoints = ["Microsoft.Storage", "Microsoft.Web"], delegation = "Microsoft.Web/serverFarms" }
+    { name = "subnet1", address_prefixes = ["10.1.0.0/24"], delegation = "Microsoft.ApiManagement/service", enforce_private_link_service_network_policies = true },
+    { name = "subnet2", address_prefixes = ["10.2.0.0/24"], service_endpoints = ["Microsoft.Sql"], enforce_private_link_endpoint_network_policies = true },
+    { name = "subnet3", address_prefixes = ["10.3.0.0/24"], service_endpoints = ["Microsoft.Storage", "Microsoft.Web"], delegation = "Microsoft.Web/serverFarms" }
   ]
 }
