7849fd6784edcd4612ab118bad539b70d201381d
Daniel Garrido Sánchez <gareda95@outlook.es>
2022-03-26 08:59:20 +0100

    Fix  subnet reference

--- main.tf
+++ main.tf
@@ -40,6 +40,6 @@ resource "azurerm_subnet" "subnets" {
 
 resource "azurerm_subnet_route_table_association" "vnet" {
   for_each       = { for subnet in var.subnets : subnet.name => subnet if lookup(subnet, "route_table_id", null) != null }
-  subnet_id      = azurerm_subnet.vnet[each.value.name].id
+  subnet_id      = azurerm_subnet.subnets[each.value.name].id
   route_table_id = each.value.route_table_id
 }
